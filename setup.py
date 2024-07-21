37492c2c46ac87fd6a8470fbd51ac62bda47e1e1
Meghaj Agrawal <meghajcr7@gmail.com>
2024-07-21 16:38:58 -0400

    Setup File Changes

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 AUTHOR_NAME = 'MEGHAJ AGRAWAL'
-SRC_REPO = 'SRC'
-LIST_OF_REQUIREMENTS = ['streamlit']
+SRC_REPO = 'Movie_Recommendation'
+LIST_OF_REQUIREMENTS = ['streamlit','pandas','numpy','pickle','requests','tmdbv3api','os','dotenv','ast','nltk','scikit-learn']
 
 setup(
     name = SRC_REPO,
     version='0.0.1',
     author= AUTHOR_NAME,
     description= "Movie Recommender",
-    package = [SRC_REPO],
+    packages= ['Data', 'Archive'],
     python_requirements = '>=3.7',
     install_requires = LIST_OF_REQUIREMENTS
 )
